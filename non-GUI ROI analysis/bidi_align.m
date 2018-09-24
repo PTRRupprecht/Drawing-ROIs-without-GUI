414e4d8e01a58282bfc468542716d920f88c98c8
Peter Rupprecht <PTRRupprecht@users.noreply.github.com>
2018-09-24 17:09:45 +0200

    Make it readable
    Make it readable

--- non-GUI ROI analysis/bidi_align.m
+++ non-GUI ROI analysis/bidi_align.m
@@ -1,38 +1,39 @@
-% bidirectionally align 3D movie
+% Bidirectionally aligns 3D movie
+% Input: movie, an x*y*t 3D array
+% Output: movieL, an x*y*t 3D array
+
 function movieL = bidi_align(movie)
-temp = zeros(size(movie,3),1);
 
-
-cross_template = zeros(size(movie,2),1)';
-cross_template2 = zeros(size(movie,2),1)';
-for j = 1:size(movie,3)
-    j/size(movie,3)
-    for o = 1:size(movie,1)/2
-        dd1 = movie((o-1)*2+1,:,j);
-        dd2 = movie((o-1)*2+2,:,j);
-%         cross_template = cross_template + xcorr(dd1,dd2,'biased');
-%         cross_template2 = cross_template2 + xcorr(dd1,dd1,'biased');
-        cross_template = cross_template + ifftshift(ifft(conj(fft(dd1)).*fft(dd1)));
-        cross_template2 = cross_template2 + ifftshift(ifft(conj(fft(dd1)).*fft(dd2)));
+    % subpixel computation of the shift for each image of the stack
+    shifts = zeros(size(movie,3),1);
+    cross_template = zeros(size(movie,2),1)';
+    cross_template2 = zeros(size(movie,2),1)';
+    for j = 1:size(movie,3)
+        j/size(movie,3)
+        for o = 1:size(movie,1)/2
+            dd1 = movie((o-1)*2+1,:,j);
+            dd2 = movie((o-1)*2+2,:,j);
+            cross_template = cross_template + ifftshift(ifft(conj(fft(dd1)).*fft(dd1)));
+            cross_template2 = cross_template2 + ifftshift(ifft(conj(fft(dd1)).*fft(dd2)));
+        end
+        [~,C0max] = max(cross_template);
+        subpixelCorrection = (log(cross_template(C0max-1))-log(cross_template(C0max+1))) / (log(cross_template(C0max-1)) + log(cross_template(C0max+1)) - 2*log(cross_template(C0max))) / 2;
+        a = C0max + subpixelCorrection;
+        [~,C0max] = max(cross_template2);
+        subpixelCorrection = (log(cross_template2(C0max-1))-log(cross_template2(C0max+1))) / (log(cross_template2(C0max-1)) + log(cross_template2(C0max+1)) - 2*log(cross_template2(C0max))) / 2;
+        b = C0max + subpixelCorrection;
+        shifts(j) = b-a; 
     end
-    [~,C0max] = max(cross_template);
-    subpixelCorrection = (log(cross_template(C0max-1))-log(cross_template(C0max+1))) / (log(cross_template(C0max-1)) + log(cross_template(C0max+1)) - 2*log(cross_template(C0max))) / 2;
-    a = C0max + subpixelCorrection;
-    [~,C0max] = max(cross_template2);
-    subpixelCorrection = (log(cross_template2(C0max-1))-log(cross_template2(C0max+1))) / (log(cross_template2(C0max-1)) + log(cross_template2(C0max+1)) - 2*log(cross_template2(C0max))) / 2;
-    b = C0max + subpixelCorrection;
-    temp(j) = b-a; 
-end
 
-movieL = zeros(size(movie));
-
-for j = 1:size(movie,3)
-    for o = 1:size(movie,1)/2
-        movieL((o-1)*2+2,:,j) = circshift(movie((o-1)*2+2,:,j),[0 0]);
-        movieL((o-1)*2+1,:,j) = circshift(movie((o-1)*2+1,:,j),[0 round(median(temp))]);
+    % shift of every second line by the average value computed above 
+    movieL = zeros(size(movie));
+    for j = 1:size(movie,3)
+        for o = 1:size(movie,1)/2
+            movieL((o-1)*2+2,:,j) = circshift(movie((o-1)*2+2,:,j),[0 0]);
+            movieL((o-1)*2+1,:,j) = circshift(movie((o-1)*2+1,:,j),[0 round(median(shifts))]);
+        end
     end
-end
-disp(strcat('Median change:',12,num2str(median(temp)),12,'Std change:',12,num2str(std(temp))));
+    disp(strcat('Median change:',12,num2str(median(shifts)),12,'Std change:',12,num2str(std(shifts))));
 
 end
            
